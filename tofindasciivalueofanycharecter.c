5df153aebb3ace2466a971e6346be5996c39bb2f
keshav161108 <keshav161108@gmail.com>
2026-08-31 10:54:29 +0530

    Add files via upload

---
+++ tofindasciivalueofanycharecter.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    char ch;
+    printf("Enter any charecter:");
+    scanf("%c",&ch);
+    printf("The ascii value of the charecter is:%d",ch);
+    return 0;
+
+
+}
